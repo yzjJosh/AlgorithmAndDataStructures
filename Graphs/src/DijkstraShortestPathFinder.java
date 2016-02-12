a66711ac9a595795c335b1a3bb20284c35116038
Josh <yangzijiangjosh@gmail.com>
2016-02-11 18:12:03 -0600

    changes the Dijkstra algorithm

--- Graphs/src/DijkstraShortestPathFinder.java
+++ Graphs/src/DijkstraShortestPathFinder.java
@@ -15,16 +15,19 @@ public class DijkstraShortestPathFinder implements ShortestPathFinder {
 			if(e.weight < 0)
 				throw new IllegalArgumentException("Graph has negative edge "+e+"!");
 		this.v = v;
-		disTo = new double[graph.V()];
-		edgeTo = new int[graph.V()];
+		this.disTo = new double[graph.V()];
+		this.edgeTo = new int[graph.V()];
 		Arrays.fill(disTo, Double.POSITIVE_INFINITY);
 		Arrays.fill(edgeTo, -1);
-		TreeMap<Double, Set<Integer>> map = new TreeMap<Double, Set<Integer>>();
-		disTo[v] = 0;
+		disTo[v] = 0.0;
 		edgeTo[v] = v;
-		replace(map, v, 0.0, 0.0);
-		while(!map.isEmpty())
-			relax(graph, pollSmallest(map), map);
+		double[] heap = new double[graph.V()+1];
+		int[] key = new int[graph.V()+1];
+		int[] index = new int[graph.V()];
+		int size = 0;
+		size = updateKey(heap, key, index, v, 0.0, size);
+		while(size > 0)
+			size = relax(graph, poll(heap, key, index, size--), heap, key, index, size);
 	}
 
 	@Override
@@ -53,37 +56,81 @@ public class DijkstraShortestPathFinder implements ShortestPathFinder {
 		return edgeTo[w] >= 0;
 	}
 	
-	private void relax(Graph<? extends WeightedEdge> graph, int v, TreeMap<Double, Set<Integer>> map){
+	private int relax(Graph<? extends WeightedEdge> graph, int v, double[] heap, int[] key, int[] index, int size){
 		for(WeightedEdge e: graph.adj(v))
 			if(disTo[e.w] > disTo[v]+e.weight){
-				replace(map, e.w, disTo[e.w], disTo[v]+e.weight);
+				size = updateKey(heap, key, index, e.w, disTo[v]+e.weight, size);
 				disTo[e.w] = disTo[v] + e.weight;
 				edgeTo[e.w] = v;
 			}
+		return size;
 	}
 	
-	private int pollSmallest(TreeMap<Double, Set<Integer>> map){
-		double smallest = map.firstKey();
-		Set<Integer> set = map.get(smallest);
-		int res = set.iterator().next();
-		set.remove(res);
-		if(set.isEmpty()) map.remove(smallest);
-		return res;
+	private void swap(double[] heap, int[] key, int[] index, int i, int j){
+		double val = heap[i];
+		int temp = key[i];
+		heap[i] = heap[j];
+		key[i] = key[j];
+		heap[j] = val;
+		key[j] = temp;
+		index[key[i]] = i;
+		index[key[j]] = j;
 	}
 	
-	private void replace(TreeMap<Double, Set<Integer>> map, int v, double old, double new_){
-		Set<Integer> set = map.get(old);
-		if(set != null){
-			set.remove(v);
-			if(set.isEmpty())
-				map.remove(old);
-		}
-		set = map.get(new_);
-		if(set == null){
-			set = new HashSet<Integer>();
-			map.put(new_, set);
-		}
-		set.add(v);
+	private int parent(int n){
+		return n/2;
 	}
-
+	
+	private int left(int n){
+		return n*2;
+	}
+	
+	private int right(int n){
+		return left(n) + 1;
+	}
+	
+	private void swim(int n, double[] heap, int[] key, int[] index) {
+		while(n > 1){
+			int parent = parent(n);
+			if(heap[parent] > heap[n]){
+				swap(heap, key, index, n, parent);
+				n = parent;
+			}else
+				break;
+		}
+	}
+	
+	private void sink(int n, double[] heap, int[] key, int[] index, int size){
+		while(n <= size){
+			int left = left(n);
+			int right = right(n);
+			double lvalue = left<=size? heap[left]: Double.POSITIVE_INFINITY;
+			double rvalue = right<=size? heap[right]: Double.POSITIVE_INFINITY;
+			if(heap[n]<=lvalue && heap[n]<=rvalue) break;
+			else if(lvalue<rvalue) {
+				swap(heap, key, index, n, left);
+				n = left;
+			} else {
+				swap(heap, key, index, n, right);
+				n = right;
+			}
+		}
+	}
+	
+	private int poll(double[] heap, int[] key, int[] index, int size){
+		swap(heap, key, index, size, 1);
+		sink(1, heap, key, index, size-1);
+		index[key[size]] = 0;
+		return key[size];
+	}
+	
+	private int updateKey(double[] heap, int[] key, int[] index, int k, double v, int size) {
+		if(index[k] == 0){
+			index[k] = ++size;
+			key[size] = k;
+		}
+		heap[index[k]] = v;
+		swim(index[k], heap, key, index);
+		return size;
+	}
 }
